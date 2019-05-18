1764c9d94d948f5b7e98dc07a3773377474dffed
Anwesh43 <anweshthecool0@gmail.com>
2019-05-19 01:17:45 +0530

    adding Canvas extension method

--- arcrightlineview/src/main/java/com/anwesh/uiprojects/arcrightlineview/ArcRightLineView.kt
+++ arcrightlineview/src/main/java/com/anwesh/uiprojects/arcrightlineview/ArcRightLineView.kt
@@ -11,6 +11,7 @@ import android.content.Context
 import android.graphics.Paint
 import android.graphics.Color
 import android.graphics.Canvas
+import android.graphics.RectF
 
 val nodes : Int = 5
 val lines : Int = 2
@@ -30,4 +31,35 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawArcRight(size : Float, sc : Float, paint : Paint) {
+    drawArc(RectF(-size / 2, -size / 2, size / 2, size / 2), 90f, 90f * sc, false, paint)
+}
+
+fun Canvas.drawRightLines(i : Int, size : Float, sc : Float, paint : Paint) {
+    save()
+    rotate(90f * i)
+    drawLine(0f, size / 2, 0f, size / 2 + size / 2 * sc.divideScale(i, lines), paint)
+    restore()
+}
+
+fun Canvas.drawARLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.style = Paint.Style.STROKE
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawArcRight(size, sc1, paint)
+    for (j in 0..(lines - 1)) {
+        drawRightLines(j, size, sc2, paint)
+    }
+    restore()
+}
 
