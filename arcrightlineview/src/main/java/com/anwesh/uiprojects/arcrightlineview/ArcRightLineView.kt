7bb56da7297c5c90a67943b70231ab4b833e3f70
Anwesh43 <anweshthecool0@gmail.com>
2019-05-19 01:02:00 +0530

    adding constants and libraries

--- arcrightlineview/src/main/java/com/anwesh/uiprojects/arcrightlineview/ArcRightLineView.kt
+++ arcrightlineview/src/main/java/com/anwesh/uiprojects/arcrightlineview/ArcRightLineView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.arcrightlineview
  * Created by anweshmishra on 19/05/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val lines : Int = 2
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
+
