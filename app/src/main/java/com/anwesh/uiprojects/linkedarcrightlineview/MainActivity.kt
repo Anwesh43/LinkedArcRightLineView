cc28ef2ba30a29a2ee8c682785d9cab7319f67ac
Anwesh43 <anweshthecool0@gmail.com>
2019-05-19 01:35:42 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedarcrightlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedarcrightlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedarcrightlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.arcrightlineview.ArcRightLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ArcRightLineView.create(this)
     }
 }
